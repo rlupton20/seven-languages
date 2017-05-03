93fee3c92dd15339c5ff202158163f7430bf663b
Richard Lupton <richard.lupton@gmail.com>
2017-05-03 23:34:00 +0100

    Day 1 ruby exercises

---
+++ Ruby/guessing-game.rb
@@ -0,0 +1,27 @@
+def high_or_low(i, j)
+  if i < j
+    "high"
+  else
+    "low"
+  end
+end
+
+
+def make_a_guess
+  puts "Make a guess:"
+  gets.to_i
+end
+
+
+def main
+  i = rand(10)
+  while i != (j = make_a_guess)
+    puts "Your guess was too #{high_or_low(i,j)}"
+  end
+  puts "You guessed correct"
+end
+
+
+if __FILE__ == $0
+  main
+end
