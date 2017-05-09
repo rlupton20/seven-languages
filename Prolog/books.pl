b742db019c0dc5a06eb0c0826c450cdc6a6767d3
Richard Lupton <richard.lupton@gmail.com>
2017-05-10 00:03:08 +0100

    Prolog day 1

---
+++ Prolog/books.pl
@@ -0,0 +1,7 @@
+author(nothern_lights, pullman).
+author(lord_of_the_rings, tolkein).
+author(c_programming, kernighan).
+author(c_programming, ritchie).
+author(set_theory, kunen).
+author(set_theory, jech).
+author(multiple_forcing, jech).
---
+++ Prolog/music.pl
@@ -0,0 +1,6 @@
+instrument(steve_vai, guitar).
+instrument(dylan, guitar).
+instrument(dylan, harmonica).
+
+genre(steve_vai, rock).
+genre(dylan, political_noise).
