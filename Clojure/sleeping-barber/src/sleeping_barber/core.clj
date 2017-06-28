dcf1d59710a358940a1f0d75f465b9bfc6323981
Richard Lupton <richard.lupton@gmail.com>
2017-06-28 23:59:55 +0100

    Tidying barber problem

--- Clojure/sleeping-barber/src/sleeping_barber/core.clj
+++ Clojure/sleeping-barber/src/sleeping_barber/core.clj
@@ -5,38 +5,57 @@
 ;;; basic concurrency primitive. This ensures no person can be in two seats
 ;;; at once, and two people can't move the a single seat at the same time.
 
+;;; Run with (cut-hair)
 
-(def run (atom true))
 
-;; Define a ref for people who got their hair cut
-(def got-hair-cut (ref 0))
+(defn new-barber-shop
+  "Create the state for a new barber shop, and run
+   of the experiment"
+  []
+  (let [ run (atom true)
+         barbers-chair (ref true)
+         got-hair-cut (ref 0)
+         waiting (ref 0) ]
+    { 'run run
+     , 'barbers-chair barbers-chair
+     , 'got-hair-cut got-hair-cut
+     , 'waiting waiting }
+    ))
 
-;; Make an alias for the barbers chair being empty
-(def empty true)
 
-;; Barbers chair
-(def barbers-chair (ref empty))
+(defn is-empty
+  "Predicate for barbers chair being empty"
+  [barbers-chair]
+  barbers-chair)
 
-;; People waiting
-(def waiting (ref 0))
-(def max-waiting 3)
+
+(defn toggle
+  "Function to change state of barbers chair, from empty to full, or
+   full to empty"
+  [barbers-chair]
+  (not barbers-chair))
+
 
 (defn continuously-cut-hair
-  []
-  (loop []
+  [shop]
+  (let [ run (shop 'run)
+         barbers-chair (shop 'barbers-chair)
+         got-hair-cut (shop 'got-hair-cut)
+         waiting (shop 'waiting) ]
+    (loop []
       (dosync
-       (if (and @barbers-chair (> @waiting 0))
+       (if (and (is-empty @barbers-chair) (> @waiting 0))
          (do
            (alter waiting dec)
-           (alter barbers-chair not))))
-      (if (not @barbers-chair)
+           (alter barbers-chair toggle))))
+      (if (not (is-empty @barbers-chair))
         (do
           (Thread/sleep 20)
           (dosync
            (alter got-hair-cut inc)
-           (alter barbers-chair not))))
+           (alter barbers-chair toggle))))
       (if @run
-        (recur))))
+        (recur)))))
 
 
 (defn until-next-customer
@@ -44,29 +63,38 @@
   []
   (+ 10 (rand-int 20)))
 
+
 (defn join-or-leave
+  "When a customer arrives, should we join the waiting queue or leave"
   [queue]
-  (if (< queue max-waiting)
-    (inc queue)
-    queue))
+  (let [max-waiting 3]
+    (if (< queue max-waiting)
+      (inc queue)
+      queue)))
+
 
 (defn generate-customers
-  []
-  (loop []
-    (Thread/sleep (until-next-customer))
-    (dosync
-     (alter waiting join-or-leave))
-    (if @run
-      (recur))))
+  [shop]
+  (let [ run (shop 'run)
+         waiting (shop 'waiting) ]
+    (loop []
+      (Thread/sleep (until-next-customer))
+      (dosync
+       (alter waiting join-or-leave))
+      (if @run
+        (recur)))))
 
 
 (defn cut-hair
   []
-  (let [barber (future (continuously-cut-hair)),
-        customers (future (generate-customers))]
-    (Thread/sleep 10000)
-    ;; Cancelling futures doesn't seem robust in tight loops,
-    ;; so we use a killswitch atom
-    (let [done @got-hair-cut]
-      (swap! run not)
-      done)))
+  (let [ shop (new-barber-shop)
+         run (shop 'run)
+         got-hair-cut (shop 'got-hair-cut) ]
+    (let [barber (future (continuously-cut-hair shop)),
+          customers (future (generate-customers shop))]
+      (Thread/sleep 10000)
+      ;; Cancelling futures doesn't seem robust in tight loops,
+      ;; so we use a killswitch atom
+      (let [done @got-hair-cut]
+        (swap! run not)
+        done))))
